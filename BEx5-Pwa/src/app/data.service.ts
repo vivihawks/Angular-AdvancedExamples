0864d01a81bd90251db484e0b78f60a2a0c7aff0
Natarajan <119780@cognizant.com>
2019-07-08 14:04:13 +0530

    git

--- BEx5-Pwa/src/app/data.service.ts
+++ BEx5-Pwa/src/app/data.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DataService {
-
+//Test Comment
   constructor(private http: HttpClient) {}
 	gimmeJokes() {
 	  return this.http.get('https://api.chucknorris.io/jokes/random');
