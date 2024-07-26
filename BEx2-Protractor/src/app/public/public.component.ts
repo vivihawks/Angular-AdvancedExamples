cef3d88e46cdac5f64cd3387f225769ea6119b94
vivihawks <48426024+vivihawks@users.noreply.github.com>
2024-07-26 13:27:32 +0530

    Delete BEx2-Protractor directory

--- BEx2-Protractor/src/app/public/public.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '../_services/index';
-@Component({
-  templateUrl: './public.component.html'
-})
-export class PublicComponent implements OnInit {
-  constructor(public authenticationService: AuthenticationService) {}
-  ngOnInit() {}
-}
